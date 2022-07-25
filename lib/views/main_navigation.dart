af627676a6d3c96b23d83128667d4f3035185139
Reinvy <satriaoner@gmail.com>
2022-07-25 16:36:57 +0700

    Init flutter framework

---
+++ lib/views/main_navigation.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({Key? key}) : super(key: key);
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
